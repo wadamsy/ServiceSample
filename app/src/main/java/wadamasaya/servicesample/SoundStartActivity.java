e13ce1a3577576b9d57ba8c2e19905d2d226045a
wadamasaya <16010418@anabuki-college.ac.jp>
2018-07-31 15:10:01 +0900

    init

---
+++ app/src/main/java/wadamasaya/servicesample/SoundStartActivity.java
@@ -0,0 +1,13 @@
+package wadamasaya.servicesample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoundStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_start);
+    }
+}
